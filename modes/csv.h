e381fdbbe7259313be56f251b28aeb21aa3d3524
Matt3o0 <duvbolone@gmail.com>
2023-06-23 23:31:47 +0200

    feat: rename hi.csv to example.csv

--- modes/csv.h
+++ modes/csv.h
@@ -23,6 +23,15 @@ void csvDrawer(int argc, const char **argv)
         std::string fullScreen = repeat(line, windowSize[1]);
         std::cout << fullScreen;
 
+        if (containsArg(argc, argv, "-s"))
+        {
+            moveCursorToColRow(2, 0);
+            std::cout << "\033[0m"
+                      << "\033[1mStatus\033[0m -  cycle: \033[1;92m" << cycle << "\033[0m"
+                      << " - one cycle every \033[1;92m2 \033[0msecond"
+                      << "\033[0;40m";
+        }
+        
         for (int i = 0; i < arr.size(); ++i)
         {
             if (arr[i].size() < 3)
@@ -36,15 +45,6 @@ void csvDrawer(int argc, const char **argv)
                           << lstrip(arr[i][2])
                           << "\033[0;91;40m";
             }
-        }
-
-        if (containsArg(argc, argv, "-s"))
-        {
-            moveCursorToColRow(2, 0);
-            std::cout << "\033[0m"
-                      << "\033[1mStatus\033[0m -  cycle: \033[1;92m" << cycle << "\033[0m"
-                      << " - one cycle every \033[1;92m2 \033[0msecond"
-                      << "\033[0;40m";
         }
 
         std::string line2 = "\033[8m" + repeat("█", windowSize[0]);
